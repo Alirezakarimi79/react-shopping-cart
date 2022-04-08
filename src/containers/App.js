e977e8f3d4b25023092a6518b5ed01d9181da9c4
Alireza Karimi <79Alirezakarimi79@gamil.com>
2022-04-08 19:24:53 +0430

    feat: add redux cart in shopping cart project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,45 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Products from "../components/Products";
 import Filter from "../components/Filter";
 import Cart from "./../components/Cart";
 
 const App = () => {
-  const [cartItems, setCartItems] = useState(
-    localStorage.getItem("cartItems")
-      ? JSON.parse(localStorage.getItem("cartItems"))
-      : []
-  );
-
-  const addToCart = (product) => {
-    const items = cartItems.slice();
-    let alreadyInCart = false;
-    items.forEach((item) => {
-      if (item._id === product._id) {
-        item.count++;
-        alreadyInCart = true;
-      }
-    });
-    if (!alreadyInCart) {
-      items.push({ ...product, count: 1 });
-    }
-    setCartItems(items);
-    localStorage.setItem("cartItems", JSON.stringify(items));
-  };
-
-  const removeFromCart = (product) => {
-    const items = cartItems.slice();
-    setCartItems(items.filter((item) => item._id !== product._id));
-    localStorage.setItem(
-      "cartItems",
-      JSON.stringify(items.filter((item) => item._id !== product._id))
-    );
-  };
-
-  const createOrder = (order) => {
-    alert("Need To Save Order For " + order.name);
-  };
-
   return (
     <div className="grid-container">
       <header>
@@ -49,14 +14,10 @@ const App = () => {
         <div className="content">
           <div className="main">
             <Filter />
-            <Products addToCart={addToCart} />
+            <Products />
           </div>
           <div className="sidebar">
-            <Cart
-              cartItems={cartItems}
-              removeFromCart={removeFromCart}
-              createOrder={createOrder}
-            />
+            <Cart />
           </div>
         </div>
       </main>
--- src/redux/reducers/combineReducers.js
+++ src/redux/reducers/combineReducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import { productReducer } from "./productsReducer";
+import { cartReducer } from "./cartReducers";
 
 export default combineReducers({
     products: productReducer,
+    cart: cartReducer
 })
